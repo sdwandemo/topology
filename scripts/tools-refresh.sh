1ba622aff42b0aa86cb0a24a9726726144410baf
voobscout <voobscout@archlinux.info>
2017-08-18 16:58:39 +0200

    ..

--- scripts/tools-refresh.sh
+++ scripts/tools-refresh.sh
@@ -9,9 +9,9 @@ _docker_compose() {
     local binary=$(curl -fsSL https://api.github.com/repos/docker/compose/releases/latest | jq -r ".assets[] | .browser_download_url | select(contains(\"${version}\"))")
     curl -fsSL "${binary}" > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
-    logger -t tools-refresh -s 'Ran refresh!'
+    logger -t tools-refresh -s 'refreshed docker-compose'
 }
 
 _docker_compose
 
-logger -t tools-refresh -s finished
+logger -t tools-refresh -s 'script finished'
