0e8aa7d4b1a741ce21ebd2d66a2e5f265bfd2944
voobscout <voobscout@archlinux.info>
2017-08-18 16:49:44 +0200

    .

--- scripts/tools-refresh.sh
+++ scripts/tools-refresh.sh
@@ -6,7 +6,7 @@
 
 _docker_compose() {
     local version="$(uname -s)-$(uname -m)"
-    local binary="$(curl -fsSL https://api.github.com/repos/docker/compose/releases/latest | jq -r \".assets[] | .browser_download_url | select(contains(\\\"$version\\\"))\")"
+    local binary=$(curl -fsSL https://api.github.com/repos/docker/compose/releases/latest | jq -r ".assets[] | .browser_download_url | select(contains(\"$version\"))")
     curl -fsSL $binary > /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 }
